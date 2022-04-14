286b261c3d40d88461934af2028f05b4df491696
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2022-04-14 22:39:33 +0800

    Update Flight_Formation_lib.lua

--- lib/Flight_Formation_lib.lua
+++ lib/Flight_Formation_lib.lua
@@ -139,7 +139,8 @@ function flight_formation.formUP(e_name,ff,f,port,is_Queen) --******************
 	if is_Queen then
 		for addr,pos in pairs(ff) do
 			--modem.send(addr,port,"formup",e_name,pos[1],pos[2],pos[3])
-			local comp_pos = pos
+			local comp_pos = {}
+			comp_pos = pos
 			if pos[1]>0 then comp_pos[1]=pos[1]-1 end --compensate positive coordinate component position
 			if pos[2]>0 then comp_pos[2]=pos[2]-1 end
 			if pos[3]>0 then comp_pos[3]=pos[3]-1 end
