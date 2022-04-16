4e1538d29ac6b20c7e1f575c5afd322d4e2adbd1
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2022-04-16 17:47:48 +0800

    Update GPS_TRG_lib.lua

--- lib/GPS_TRG_lib.lua
+++ lib/GPS_TRG_lib.lua
@@ -42,6 +42,8 @@ function GPS_TRG.bcGPSTRGPos(tpBook,gpsC)
 					modem.broadcast(tport,"trg",trgPos.x,trgPos.y,trgPos.z)
 				end
 			end
+		else
+			print("GPS Out Of Range")
 		end
 		refreshGPSCounter,gpsTable = GPS.refreshGPSTable(gpsTable,refreshGPSCounter,refreshGPSInterval)
 		os.sleep(0.5)
@@ -81,6 +83,8 @@ function GPS_TRG.bcGPSTRGPosPRINT(tpBook,gpsC)
 					print(tname," is out of radar Range")
 				end
 			end
+		else
+			print("GPS Out Of Range")
 		end
 		refreshGPSCounter,gpsTable = GPS.refreshGPSTable(gpsTable,refreshGPSCounter,refreshGPSInterval)
 		os.sleep(0.5)
@@ -88,7 +92,7 @@ function GPS_TRG.bcGPSTRGPosPRINT(tpBook,gpsC)
 	
 end
 
-function GPS_TRG.bcGPSRecall(tpBook,gpsC)
+function GPS_TRG.bcGPSRecall(tpBook,gpsC,PawnsC)
 	modem.open(gpsC)
 	modem.setStrength(math.huge)
 	local gpsTable = {}
@@ -99,6 +103,7 @@ function GPS_TRG.bcGPSRecall(tpBook,gpsC)
 			GPS.add2GPSTable(r_addr,xg,yg,zg,dist,gpsTable)
 		end
 	end)
+	modem.broadcast(PawnsC,"formup",0,0,0,1)
 	while true do
 		term.clear()
 		local gpsPos = GPS.getGPSPos(gpsTable)
@@ -106,10 +111,9 @@ function GPS_TRG.bcGPSRecall(tpBook,gpsC)
 			gpsPos = vec_trunc(gpsPos)
 			print("Recalling...")
 			print("GPS Location: ",gpsPos.x,gpsPos.y,gpsPos.z)
-			for tport,tname in pairs(tpBook) do
-				print("tport:",tport,"tname:",tname)
-				modem.broadcast(tport,"trg",gpsPos.x,gpsPos.y,gpsPos.z)
-			end
+			modem.broadcast(1,"trg",gpsPos.x,gpsPos.y,gpsPos.z)
+		else
+			print("GPS Out Of Range")
 		end
 		refreshGPSCounter,gpsTable = GPS.refreshGPSTable(gpsTable,refreshGPSCounter,refreshGPSInterval)
 		os.sleep(0.5)
@@ -129,9 +133,9 @@ function GPS_TRG.updateGPSTRGsPRINT(tpBook,gpsC) --**********************-- --on
 	GPS_TRG.gpstrgThread = thread.create(function(tpb,gpsC) print("threading") GPS_TRG.bcGPSTRGPosPRINT(tpb,gpsC) end,tpBook,gpsC)
 end
 
-function GPS_TRG.GPSRecall(tpBook,gpsC) --**********************-- --only call this sparingly, don't want to stall other flight formations
+function GPS_TRG.GPSRecall(tpBook,gpsC,PawnsC) --**********************-- --only call this sparingly, don't want to stall other flight formations
 	GPS_TRG.killGPSTRGThread(gpsC)
-	GPS_TRG.gpstrgThread = thread.create(function(tpb,gpsC) print("threading") GPS_TRG.bcGPSRecall(tpb,gpsC) end,tpBook,gpsC)
+	GPS_TRG.gpstrgThread = thread.create(function(tpb,gpsC,PawnsC) print("threading") GPS_TRG.bcGPSRecall(tpb,gpsC,PawnsC) end,tpBook,gpsC,PawnsC)
 end
 
 function GPS_TRG.killGPSTRGThread(gpsC) --**********************--
