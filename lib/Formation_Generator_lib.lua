f38b094fa94b02ecfea449ef0f846bcfd035f374
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2022-04-23 13:37:26 +0800

    Update Formation_Generator_lib.lua

--- lib/Formation_Generator_lib.lua
+++ lib/Formation_Generator_lib.lua
@@ -282,7 +282,7 @@ function FORMATION_GENERATOR.TriangleFormation(plane_axis,height,base,scale,base
         table.insert(formationTable,{pos.x,pos.y,pos.z})
         table.insert(formationTable,{neg.x,neg.y,neg.z})
     end
-    for b = run,1,-1 do 
+    for b = run-1,1,-1 do 
         local pos,neg = triangleDirection[plane_axis](height,b,basePoint)
 	
         if not s_utils.isEqual(pos,neg) then
