856a979dc8150a7bc9a51e65e430272456869123
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2022-04-23 12:10:04 +0800

    Update Swarm_Client.lua

--- bin/Swarm_Client.lua
+++ bin/Swarm_Client.lua
@@ -93,7 +93,7 @@ Pawnfbook = {dynamicTriangle,staticTriangle,staticOrbitSquare,dynamicOrbitSquare
 Pawndynamic_fbook = Pawnfbook
 
 ring = formation_generator.circleFormation("Y",10,{x=0,y=0,z=7})
-hollow_square = formation_generator.hollowSquareFormation("Y",3,3,2,{x=-2,y=0,z=-1})
+hollow_square = formation_generator.hollowSquareFormation("Y",3,3,2,{x=-6,y=0,z=-6})
 PawnGeneratedFormBook = {ring,hollow_square}
 dynamicPawnPyramidBook = PawnGeneratedFormBook
 
