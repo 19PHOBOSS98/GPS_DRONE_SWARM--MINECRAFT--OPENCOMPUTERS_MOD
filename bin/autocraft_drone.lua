cd966a859ecf88882ad2c753d98e452550512cc0
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2022-04-25 21:32:38 +0800

    Update autocraft_drone.lua

--- bin/autocraft_drone.lua
+++ bin/autocraft_drone.lua
@@ -1,6 +1,28 @@
 local component = require("component")
 local inventory_controller = component.inventory_controller
 local robot = component.robot
+local modem = component.modem
+local PCchannel = 10000
 
-local item = robot.select(2)
+robot.select(1)
 inventory_controller.dropIntoSlot(3,1,1)
+robot.select(2)
+inventory_controller.dropIntoSlot(3,5,1)
+robot.select(3)
+inventory_controller.dropIntoSlot(3,14,1)
+robot.select(4)
+inventory_controller.dropIntoSlot(3,17,1)
+robot.select(5)
+inventory_controller.dropIntoSlot(3,18,1)
+robot.select(6)
+inventory_controller.dropIntoSlot(3,19,1)
+robot.select(7)
+inventory_controller.dropIntoSlot(3,20,1)
+
+
+robot.select(8)
+inventory_controller.dropIntoSlot(3,7,1)
+robot.select(9)
+inventory_controller.dropIntoSlot(3,5,1)
+
+modem.broadcast(PCchannel,"craft")
