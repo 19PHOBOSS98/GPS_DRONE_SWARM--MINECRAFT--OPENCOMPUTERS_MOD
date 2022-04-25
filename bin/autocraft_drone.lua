c5c31a68abc30353499191970c6b9e2e40ffeb5c
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2022-04-25 19:22:53 +0800

    Update autocraft_drone.lua

--- bin/autocraft_drone.lua
+++ bin/autocraft_drone.lua
@@ -1,5 +1,6 @@
 local component = require("component")
 local inventory_controller = component.inventory_controller
+local robot = component.robot
 
-local item = inventory_controller.getStackInInternalSlot(1)
+local item = robot.select(2)
 inventory_controller.dropIntoSlot(3,1,1)
